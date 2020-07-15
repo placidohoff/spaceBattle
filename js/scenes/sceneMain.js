4c1a7d9650b3f635f7647d0b4514ec7d65b57055
placidohoff <sweetflow401@gmail.com>
2020-07-15 13:01:36 -0400

    Stop Moving Works, Adjusted for scale

--- js/scenes/sceneMain.js
+++ js/scenes/sceneMain.js
@@ -21,17 +21,31 @@ class SceneMain extends Phaser.Scene{
         this.background.setOrigin(0,0)
         this.ship = this.physics.add.sprite(this.centerX, this.centerY, 'ship');
 
+        //Scale down the ship to 25%/2 of the width:
+        Align.scaleToGameW(this.ship, .125)
+
+        //Set the background to same scale as ship:
+            //We do not use Align.scaleToGameW() since background will be scrolling.
+        this.background.scaleX = this.ship.scaleX;
+        this.background.scaleY = this.ship.scaleY;
+
         this.background.setInteractive();
         this.background.on('pointerdown', this.backgroundClicked, this)
     }
     update(){
-
+        //Measure the distance to click point to know when to stop ship:
+            let distX = Math.abs(this.ship.x - this.tx);
+            let distY = Math.abs(this.ship.y-this.ty);
+            if(distX < 10 && distY < 10){
+                this.ship.body.setVelocity(0,0);
+            }
     }
     backgroundClicked(){
-        let tx = this.background.input.localX;
-        let ty = this.background.input.localY;
+        //Had to multiply the 'tx', 'ty' by 'this.scaleX' because this scale down changed greatly effected the code:
+            this.tx = this.background.input.localX * this.background.scaleX;
+            this.ty = this.background.input.localY * this.background.scaleY;
 
-        let angle = this.physics.moveTo(this.ship, tx, ty, 90);
+        let angle = this.physics.moveTo(this.ship, this.tx, this.ty, 90);
         angle = this.toDegrees(angle)
         this.ship.angle = angle;
     }
